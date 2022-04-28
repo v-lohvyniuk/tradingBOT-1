dd114e551c0094f28f9d8b57ee0a98ce8be06b90
v-lohvyniuk <volodymyr.lohvyniuk@gmail.com>
2022-04-29 00:36:14 +0300

    Fix typo

--- db/dbconfig.py
+++ db/dbconfig.py
@@ -1,6 +1,6 @@
 import os
 
-DB_URL = os.environ['DATABASE_URL'].replace("posrgres", "postgresql")
+DB_URL = os.environ['DATABASE_URL'].replace("postgres", "postgresql")
 print("Here are my creds la la la " + DB_URL)
 # DB_URL = "postgresql://ajrduocxletvlv:20c1c4981cba66df703efd82959a45938665988f3262dd073090852134db4750@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/dstlhpucamj4"
 DB_PORT = "5432"
