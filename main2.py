22b5171f1f107df3a56590082447b78e94814dc3
v-lohvyniuk <volodymyr.lohvyniuk@gmail.com>
2022-05-02 20:15:34 +0300

    Fix startup issue

--- main2.py
+++ main2.py
@@ -220,7 +220,6 @@ def flush():
         return redirect(url_for("root"))
 
 
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5201))
-#     app.run(host='0.0.0.0', port=port)
-#
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5201))
+    app.run(host='0.0.0.0', port=port)
